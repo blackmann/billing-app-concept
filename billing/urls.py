8f95887322a8f3c5a5d09e0dc1351e4ba7f2fffa
DeGreat Yartey <yarteydegreat2@gmail.com>
2018-11-28 21:57:46 +0000

    Fix to staticfiles

--- billing/urls.py
+++ billing/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.static import serve
+from billing import settings
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^static/(?P<path>.*)$', serve, {
+                            'document_root': settings.STATIC_ROOT, }),
 ]
